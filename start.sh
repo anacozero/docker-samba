ff87320920c58291733eb772eaeecb2289dd7b88
anacozero <anaco7@hotmail.com>
2020-05-10 19:09:20 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+cp /etc/hosts ~/hosts.new
+sed -i "/127.0.0.1.*/a 127.0.0.1       $HOSTNAME.$REALM $HOSTNAME" ~/hosts.new
+cp ~/hosts.new /etc/hosts
+
 echo $PASS | kinit -V $UNAME@$REALM
 net ads join -U"$UNAME"%"$PASS"
 
